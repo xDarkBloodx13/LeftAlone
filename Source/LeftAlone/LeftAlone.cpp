48eb17c6e94f3d187b47888c062a1d638db161f6
Jerry Bruce <Bruc3y33@gmail.com>
2020-09-28 08:33:38 -0500

    Initial commit

---
+++ Source/LeftAlone/LeftAlone.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LeftAlone.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LeftAlone, "LeftAlone" );
---
+++ Source/LeftAlone/LeftAloneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeftAloneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEFTALONE_API ALeftAloneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
